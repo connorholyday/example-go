bb7b75f5b5769156848883f06636e7a0f8f509c6
Connor Holyday <connorholyday@gmail.com>
2018-04-27 23:29:06 +0100

    Add Spanish support

--- hello.go
+++ hello.go
@@ -5,17 +5,22 @@ import (
 )
 
 const helloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(input string) string {
+func Hello(input string, language string) string {
     suffix := "world"
 
     if input != "" {
         suffix = input
+    }
+    
+    if language == "Spanish" {
+        return spanishHelloPrefix + suffix
     }
 
     return helloPrefix + suffix
 }
 
 func main() {
-    fmt.Println(Hello("world"))
+    fmt.Println(Hello("world", "English"))
 }
--- hello_test.go
+++ hello_test.go
@@ -15,16 +15,23 @@ func TestHello(t *testing.T) {
 
     t.Run("Saying hello to people", func(t *testing.T) {
         input := "YOU"
-        got := Hello(input)
+        got := Hello(input, "English")
         want := "Hello, " + input
         assertCorrectMessage(t, got, want)
     })
 
     t.Run("Saying hello world when an empty string is supplied", func(t *testing.T) {
         input := ""
-        got := Hello(input)
+        got := Hello(input, "English")
         want := "Hello, world"
         assertCorrectMessage(t, got, want)
     })
+
+    t.Run("Saying hello in Spanish", func(t *testing.T) {
+        got := Hello("Elodie", "Spanish")
+        want := "Hola, Elodie"
+        assertCorrectMessage(t, got, want)
+    })
+
 
 }
