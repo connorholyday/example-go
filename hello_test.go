9e457544e3aa9b3519c7ed776725cb12caa022a9
Connor Holyday <connorholyday@gmail.com>
2018-04-27 23:05:08 +0100

    Extract the test assertion into helper function

--- hello_test.go
+++ hello_test.go
@@ -5,11 +5,19 @@ import (
 )
 
 func TestHello(t *testing.T) {
-    input := "YOU"
-    got := Hello(input)
-    want := "Hello, " + input
 
-    if got != want {
-        t.Errorf("got '%s' want '%s'", got, want)
+    assertCorrectMessage := func(t *testing.T, got, want string) {
+        t.Helper()
+        if got != want {
+            t.Errorf("got '%s' want '%s'", got, want)
+        }
     }
+
+    t.Run("Saying hello to people", func(t *testing.T) {
+        input := "YOU"
+        got := Hello(input)
+        want := "Hello, " + input
+        assertCorrectMessage(t, got, want)
+    })
+
 }
